baa23fe5073a9e0d780db777d8f94262e0d584a9
ah9mon <eomgyeol97@gmail.com>
2023-07-12 21:34:45 +0900

    230712

--- Hangyeol/_23_07_12/FoodFight.java
+++ Hangyeol/_23_07_12/FoodFight.java
@@ -1,18 +1,20 @@
 public class FoodFight {
-    public int solution(int[] ingredient) {
-        int[] stack = new int[ingredient.length];
-        int sp = 0;
-        int answer = 0;
-        for (int i : ingredient) {
-            stack[sp++] = i;
-            if (sp >= 4 && stack[sp - 1] == 1
-                    && stack[sp - 2] == 3
-                    && stack[sp - 3] == 2
-                    && stack[sp - 4] == 1) {
-                sp -= 4;
-                answer++;
+    public StringBuilder solution(int[] food) {
+        StringBuilder answer = new StringBuilder();
+
+        // 한 선수가 먹을 음식 순서 생성
+        for (int foodNumber = 1; foodNumber < food.length; foodNumber++) {
+            int numberOfFood = food[foodNumber] / 2;
+            for (int count = 0; count < numberOfFood; count++) {
+                answer.append(foodNumber);
             }
         }
+
+        // 다른 선수가 먹을 음식도 세팅
+        StringBuilder reverseAnswer = new StringBuilder(answer);
+        reverseAnswer.reverse();
+        answer.append("0"); // 가운데 물놓기
+        answer.append(reverseAnswer);
         return answer;
     }
 }
