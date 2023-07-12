e040cafc94ab162551bf79bc014ef5b90ed0445d
haegu97 <haegu97@naver.com>
2023-07-12 20:58:08 +0900

    230708_1

--- sungu/programmers_5week/5_4.py
+++ sungu/programmers_5week/5_4.py
@@ -1,3 +1,9 @@
+#combination 라이브러리를 사용하면 조합을 알아서 계산해줌
+#Counter 라이브러리 사용하면 조합의 알파벳 개수를 세어줌
+#orders에서 하나씩 꺼내와서 course 길이로 조합을 짜서 temp에 넣어줌
+#
+
+
 from itertools import combinations
 from collections import Counter
 
@@ -14,7 +20,7 @@ def solution(orders, course):
             
         counter = Counter(temp)
         
-        if len(counter) != 0 and max(counter.values()) != 1:
-            answer += [''.join(f) for f in counter if counter[f] == max(counter.values())]
+        if len(counter) != 0 and max(counter.values()) != 1:  #2개 이상인 것들만 세어줌
+            answer += [''.join(f) for f in counter if counter[f] == max(counter.values())]  #가장 많이 나온 조합이면 answer에 더해줌
 
     return sorted(answer)
